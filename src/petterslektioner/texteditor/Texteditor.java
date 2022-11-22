3612755606ef6f08ca266caae9bdf1f9660264f9
Victor Andersson <victor.andersson2@proton.me>
2022-11-22 12:38:21 +0100

    Better documentation of the code

--- src/petterslektioner/texteditor/Texteditor.java
+++ src/petterslektioner/texteditor/Texteditor.java
@@ -4,10 +4,13 @@
  */
 package petterslektioner.texteditor;
 
+import javax.swing.*;
+
 /**
  *
  * @author victor
  */
+@SuppressWarnings({"Convert2Lambda", "Anonymous2MethodRef", "unused", "FieldCanBeLocal"})
 public class Texteditor extends javax.swing.JFrame {
 
     /**
@@ -209,13 +212,8 @@ public class Texteditor extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Texteditor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Texteditor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Texteditor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                 UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Texteditor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
